77c178a2b7ece6254018db01a12376a956a2aaee
izumi takanori <takatantaka@gmail.com>
2018-08-18 15:43:22 +0900

    index修正

--- web/index.php
+++ web/index.php
@@ -1,5 +1,24 @@
 <?php
 
-echo "Hello World!";
+// Composerでインストールしたライブラリを一括読み込み
+require_once __DIR__ . '/composer/autoload_real.php';
+
+// アクセストークンを使いCurlHTTPClientをインスタンス化
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+
+// CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getnv('CHANNEL_SECRET')]);
+
+// LINE Messaging APIがリクエストに付与した署名を取得
+$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+
+// 署名が正当かチェック。政党であればリクエストをパースし配列へ
+$events = $bot->parseEventRequest(filie_get_contents('php://input'),$signature);
+
+// 配列に格納された各イベントをループで処理
+foreach($events as $event){
+  // テキストを返信
+  $bot->replyText($event->getReplyToken(), 'TextMessage');
+}
 
 ?>
